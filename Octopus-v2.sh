c542a38323288993570fe9e4031848d2745f5a6f
UnknowUser50 <66477027+UnknowUser50@users.noreply.github.com>
2020-07-21 10:45:05 +0200

    Update Octopus-v2.sh

--- Octopus-v2.sh
+++ Octopus-v2.sh
@@ -328,11 +328,11 @@ read zenrsp
 
 	elif [ $zenrsp == $Non ]; then
 	printf "\e[31m [+] Starting installation ... \e[0m\n"
-	sudo wget -q https://nmap.org/dist/zenmap-7.80-1.noarch.rmp &>/dev/null
+	sudo wget -q https://nmap.org/dist/zenmap-7.80-1.noarch.rpm &>/dev/null
 	printf "\e[32m [+] Receiving data from nmap.org ... \e[0m\n"
 	sudo apt-get install alien &>/dev/null
 	printf "\e[32m [+] Install alien ... \e[0m\n"
-	alien zenmap_7.80-1.noarch.rmp &>/dev/null
+	alien zenmap_7.80-1.noarch.rpm &>/dev/null
 	dpkg -i zenmap_7.80-2_all.deb &>/dev/null
 	printf "\e[32m [+] Unpacking of ZenMap ... soon finishied \e[0m\n"
 	sleep 1
