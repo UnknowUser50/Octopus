33998bad83666c82ff856925e7152cb11d3fec86
UnknowUser50 <66477027+UnknowUser50@users.noreply.github.com>
2020-09-20 00:42:47 +0200

    Small update
    update

--- update.sh
+++ update.sh
@@ -35,7 +35,7 @@ backup() {
 
 if [ -d /home/$current_user/Backup-Octopus ]; then
 printf "\e[31m[\e[33m!\e[31m]\e[0m A backup directory already exist, the script will only moove file ... \e[0m\n"
-cd /home/$current_user/Octopus/
+cd /home/$current_user/Octopus/ &>/dev/null
 
 # Moove external files
 cd /home/$current_user/Octopus/ &>/dev/null
@@ -46,49 +46,49 @@ sleep 1
 
 # Moove file(s) of Network-Infos
 if [ -d /home/$current_user/Octopus/Network-Infos ]; then
-cd Network-Infos/
+cd Network-Infos/ &>/dev/null
 mv * /home/$current_user/Backup-Octopus/Network-Infos/ &>/dev/null
 printf "\e[32m[\e[33m*\e[32m] File(s) of Network-infos are saved ...\n"
-cd && cd /home/$current_user/Octopus/
+cd && cd /home/$current_user/Octopus/ &>/dev/null
 else
 printf "\e[31m[\e[33m!\e[31m] Unknow directory or non-existant ...\n"
 sleep 1
-cd /home/$current_user/Octopus/
+cd /home/$current_user/Octopus/ &>/dev/null
 fi
 
 # Moove file(s) of Network-Scan
 if [ -d /home/$current_user/Backup-Octopus/Network-Scan ]; then
-cd Network-Scan/
+cd Network-Scan/ &>/dev/null
 mv * /home/$current_user/Backup-Octopus/Network-Scan/ &>/dev/null
 printf "\e[32m[\e[33m*\e[32m] File(s) of Network-Scan are saved ...\n"
-cd && cd /home/$current_user/Octopus/
+cd && cd /home/$current_user/Octopus/ &>/dev/null
 else
 printf "\e[31m[\e[33m!\e[31m] Unknow directory or non-existant ...\n"
 sleep 1
-cd /home/$current_user/Octopus/
+cd /home/$current_user/Octopus/ &>/dev/null
 fi
 
 # Moove file(s) of Web-Scan
 if [ -d /home/$current_user/Backup-Octopus/Web-Scan ]; then
-cd Web-Scan/
+cd Web-Scan/ &>/dev/null
 mv * /home/$current_user/Backup-Octopus/Web-Scan/ &>/dev/null 
 printf "\e[32m[\e[33m*\e[32m] File(s) of Web-Scan are saved ... \n"
 cd
 else
 printf "\e[31m[\e533m!\e[31m] Unknow directory or non-existant ...\n"
 sleep 1
-cd
+cd 
 fi
 
 else
-mkdir /home/$current_user/Backup-Octopus
-cd /home/$current_user/Octopus/
-mv Network-Infos/ /home/$current_user/Backup-Octopus/ 
-mv Network-Scan/ /home/$current_user/Backup-Octopus/
-mv Web-Scan/ /home/$current_user/Backup-Octopus/
+mkdir /home/$current_user/Backup-Octopus &>/dev/null
+cd /home/$current_user/Octopus/ &>/dev/null
+mv Network-Infos/ /home/$current_user/Backup-Octopus/ &>/dev/null 
+mv Network-Scan/ /home/$current_user/Backup-Octopus/ &>/dev/null
+mv Web-Scan/ /home/$current_user/Backup-Octopus/ &>/dev/null
 sleep 1
 printf "\e[32m[\e[33m*\e[32m] All directories and files have been saved in \e[33m/%s/Backup-Octopus/ \e[0m\n" $current_user
-cd /home/$current_user
+cd /home/$current_user &>/dev/null
 fi
 
 }
@@ -99,25 +99,27 @@ if [ -d /home/$current_user/Octopus ]; then
 printf "\e[31m[\e[33m!\e[31m] An old version have been found, we will delete it ... \e[0m\n"
 cd /home/$current_user/Octopus/
 sudo rm -rf Octopus-v2.sh &>/dev/null
-sudo rm -rf README.txt &>/dev/null
-sudo rm -rf update.sh
-sudo rm -rf *
+sudo rm -rf README.md &>/dev/null
+sudo rm -rf update.sh &>/dev/nul
+sudo rm -rf * &>/dev/null
 cd /home/$current_user/
-rm -r /home/$current_user/Octopus/
+rm -r /home/$current_user/Octopus/ &>/dev/null
 
+# If error -->
 if [ -d /home/$current_user/Octopus ]; then
 printf "\e[31m[!] An error has occured, please restart the script ... \e[0m\n" 
 sleep 1
 exit
 
+# Good
 else
 printf "\e[32m[\e[33m*\e[32m] All file(s) have been deleted ... \e[0m\n"
 sleep 1
 fi
 
-# Download new version
+# Download new version of Octopus
 git clone https://github.com/UnknowUser50/Octopus &>/dev/null
-cd /home/$current_user/Octopus/
+cd /home/$current_user/Octopus/ &>/dev/null
 chmod 755 * 
 if [ -d /home/$current_user/Octopus ]; then
 printf "\e[32m[+] Update is complete ! \e[0m\n"
@@ -129,6 +131,8 @@ fi
 fi
 cd /home/$current_user/
 rm -r Octopus/
+cd &>/dev/null
+cd Octopus/ &>/dev/null
 
 }
 
