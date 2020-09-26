a770d0060cb8b0073ad68c385f3f1864bf5e82bc
UnknowUser50 <66477027+UnknowUser50@users.noreply.github.com>
2020-09-26 18:39:25 +0200

    Update Subsystem_WIN.sh

--- Subsystem_WIN.sh
+++ Subsystem_WIN.sh
@@ -12,3 +12,39 @@
 #
 ##########################################################################################################################################
 
+# Colors declaration : 
+export BLUE='\033[1;94m'
+export RED='\033[1;91m'
+export YELLOW='\033[1;93m'
+export GREEN='\033[1;92m'
+export RESETCOLOR='\033[1;00m'
+
+# Informations :
+sys_name=$(uname -a | grep "Linux *" | cut -d# -f1)
+hostname=$(hostname)
+
+run_as_root() {
+  
+  if [[ $UID != 0 ]]; then
+	printf "$YELLOW[$RED!$YELLOW] Please, run this script as $REdsudo$RESETCOLOR\n"
+	printf "$YELLOW[$RED!$YELLOW] sudo $0 $*\n"
+	# Exit error code
+	exit 1
+ 
+}
+
+bannerscreen() {
+
+printf "$BLUE     .=====================================================.                     \n"
+printf "$BLUE     ||                                                   ||                     \n"
+printf "$BLUE     ||   _   $RED    _--'$GREEN'--_                                ||           \n"
+printf "$BLUE     ||     ' $RED--''   |$GREEN    |   .--.           |    ||      ||           \n" 
+printf "$BLUE     ||   ' . $RED_|     |$GREEN    |  |    |          |    ||      ||           \n"
+printf "$BLUE     ||   _    $RED|  _--'$GREEN'--_|  |----| |.-  .-i |.-. ||      ||           \n"
+printf "$BLUE     ||     ' $BLUE--''   |$YELLOW    |  |    | |   |  | |  |         ||         \n"
+printf "$BLUE     ||   ' $BLUE. _|     |$YELLOW    |  |    | |    `-( |  | ()      ||         \n"
+printf "$BLUE     ||   $BLUE_    |  _--'$YELLOW'--_|             |  |              ||         \n"
+printf "$BLUE     ||   $BLUE  ' --''                      `--'              ||                \n"
+printf "$BLUE     ||                                                   ||                     \n"
+printf "$BLUE     `=====================================================`                     \n"
+sleep 10
