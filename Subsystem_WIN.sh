4be4bbaa3e382328460e18502f923a1db0ac931d
UnknowUser50 <66477027+UnknowUser50@users.noreply.github.com>
2020-09-26 18:28:45 +0200

    Script for dependencies on WIN subsystems
    v1

---
+++ Subsystem_WIN.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+########################################################################################################################################
+#
+# This script is made for installing the dependencies necessary for Octopus for Windows systems which run Kali Linux from the subsystem.
+#
+# @Author : UnknowUser50
+#
+# @For : Octopus script
+#
+# @Release : September 2020
+#
+##########################################################################################################################################
+
