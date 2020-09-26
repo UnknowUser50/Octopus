9f19c9992a556180cb81ccf1c91f55bcbc8785d8
UnknowUser50 <66477027+UnknowUser50@users.noreply.github.com>
2020-09-26 14:03:24 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+
 daate=$(date +%c)
+export BLUE='\033[1;94m'
+export GREEN='\033[1;92m'
+export RED='\033[1;91m'
+export YELLOw='\033[1;93m'
+export RESETCOLOR='\033[1;00m'
 
 preface() {
 
 cd
-command -v git > /dev/null 2>&1 || { echo -e "\e[31m[!] This script requieres git but it's not installed. Install it"; exit 1; }
-echo -e -n "\e[32m[\e[33m*\e[32m] Enter current user : \e[0m"
+command -v git > /dev/null 2>&1 || { echo -e "$RED[$YELLOW!$RED] This script requieres git but it's not installed. Install it"; exit 1; }
+echo -e -n "$BLUE[$GREEN*$BLUE] Enter current user : \e[0m"
 read current_user
 
 }
@@ -15,19 +21,19 @@ banner() {
 clear
 # Run this script as sudo or end 
 if [[ $UID != 0 ]]; then
-	printf "\e[33m[\e[31m!\e[33m] Please, run this script as \e[31msudo\e[33m:\n"
-	printf "\e[33m[\e[31m!\e[33m] sudo $0 $*\n"
+	printf "$RED[$YELLOW!$RED] Please, run this script as \e[31msudo\e[33m:\n"
+	printf "$RED[$YELLOW!$RED] sudo $0 $*\n"
 	# Exit error code
 	exit 1
 fi	
-printf "\e[33m            _                   _       _         \n"
-printf "\e[33m           | |                 | |     | |        \n"
-printf "\e[33m  ___   ___| |_ _   _ _ __   __| | __ _| |_ ___   \n"
-printf "\e[33m / _ \ / __| __| | | | '_ \ / _' |/ _' | __/ _ \  \n"
-printf "\e[33m| (_) | (__| |_| |_| | |_) | (_| | (_| | ||  __/  \n"
-printf "\e[33m \___/ \___|\__|\__,_| .__/ \__,_|\__,_|\__\___|  \n"
-printf "\e[33m                     | |                          \n"
-printf "\e[33m                     |_|                          \n"
+printf "$BLUE            _                   _       _         \n"
+printf "$BLUE           | |                 | |     | |        \n"
+printf "$BLUE  ___   ___| |_ _   _ _ __   __| | __ _| |_ ___   \n"
+printf "$BLUE / _ \ / __| __| | | | '_ \ / _' |/ _' | __/ _ \  \n"
+printf "$BLUE| (_) | (__| |_| |_| | |_) | (_| | (_| | ||  __/  \n"
+printf "$BLUE \___/ \___|\__|\__,_| .__/ \__,_|\__,_|\__\___|  \n"
+printf "$BLUE                     | |                          \n"
+printf "$BLUE                     |_|                          \n"
 printf "\n"
 
 }
@@ -35,24 +41,24 @@ printf "\n"
 backup() {
 
 if [ -d /home/$current_user/Backup-Octopus ]; then
-printf "\e[31m[\e[33m!\e[31m]\e[0m A backup directory already exist, the script will only moove file ... \e[0m\n"
+printf "$RED[$YELLOW!$RED]$RED A backup directory already exist, the script will only moove file ... \e[0m\n"
 cd /home/$current_user/Octopus/ &>/dev/null
 
 # Moove old files to a backup directory
 cd /home/$current_user/Octopus/ &>/dev/null
 mv *.txt /home/$current_user/Backup-Octopus/ &>/dev/null
 sleep 1
-printf "\e[32m[\e[33m*\e[32m] External file(s) saved in \e[33m/home/$current_user/Backup-Octopus/\e[32m...\e[0m\n"
+printf "$BLUE[$GREEN*$BLUE] External file(s) saved in $GREEN/home/$current_user/Backup-Octopus/$BLUE ...\e[0m\n"
 sleep 1
 
 # Moove file(s) of Network-Infos
 if [ -d /home/$current_user/Octopus/Network-Infos ]; then
 cd Network-Infos/ &>/dev/null
 mv * /home/$current_user/Backup-Octopus/Network-Infos/ &>/dev/null
-printf "\e[32m[\e[33m*\e[32m] File(s) of Network-infos are saved at \e[33m$daate\e[32m...\n"
+printf "$BLUE[$GREEN*$BLUE] File(s) of Network-infos are saved at $GREEN$daate$BLUE ...\n"
 cd && cd /home/$current_user/Octopus/ &>/dev/null
 else
-printf "\e[31m[\e[33m!\e[31m] Unknow directory or non-existant ...\n"
+printf "$RED[$YELLOW!$RED] Unknow directory or non-existant ...\n"
 sleep 1
 cd /home/$current_user/Octopus/ &>/dev/null
 fi
@@ -61,10 +67,10 @@ fi
 if [ -d /home/$current_user/Backup-Octopus/Network-Scan ]; then
 cd Network-Scan/ &>/dev/null
 mv * /home/$current_user/Backup-Octopus/Network-Scan/ &>/dev/null
-printf "\e[32m[\e[33m*\e[32m] File(s) of Network-Scan are saved at \e[33m$daate\e[32m...\n"
+printf "$BLUE[$GREEN*$BLUE] File(s) of Network-Scan are saved at $GREEN$daate$BLUE ...\n"
 cd && cd /home/$current_user/Octopus/ &>/dev/null
 else
-printf "\e[31m[\e[33m!\e[31m] Unknow directory or non-existant ...\n"
+printf "$RED[$YELLOW!$RED] Unknow directory or non-existant ...\n"
 sleep 1
 cd /home/$current_user/Octopus/ &>/dev/null
 fi
@@ -73,10 +79,10 @@ fi
 if [ -d /home/$current_user/Backup-Octopus/Web-Scan ]; then
 cd Web-Scan/ &>/dev/null
 mv * /home/$current_user/Backup-Octopus/Web-Scan/ &>/dev/null 
-printf "\e[32m[\e[33m*\e[32m] File(s) of Web-Scan are saved at \e[33m$daate\e[32m... \n"
+printf "$BLUE[$GREEN*$BLUE] File(s) of Web-Scan are saved at $GREEN$daate$BLUE ... \n"
 cd
 else
-printf "\e[31m[\e533m!\e[31m] Unknow directory or non-existant ...\n"
+printf "$RED[$YELLOW!$RED] Unknow directory or non-existant ...\n"
 sleep 1
 cd 
 fi
@@ -88,7 +94,7 @@ mv Network-Infos/ /home/$current_user/Backup-Octopus/ &>/dev/null
 mv Network-Scan/ /home/$current_user/Backup-Octopus/ &>/dev/null
 mv Web-Scan/ /home/$current_user/Backup-Octopus/ &>/dev/null
 sleep 1
-printf "\e[32m[\e[33m*\e[32m] All directories and files have been saved in \e[33m/%s/Backup-Octopus/ \e[0m\n" $current_user
+printf "$BLUE[$GREEN*$BLUE] All directories and files have been saved in $GREEN/%s/Backup-Octopus/ \e[0m\n" $current_user
 cd /home/$current_user &>/dev/null
 fi
 
@@ -97,7 +103,7 @@ fi
 update() {
 
 if [ -d /home/$current_user/Octopus ]; then
-printf "\e[31m[\e[33m!\e[31m] An old version have been found, we will delete it ... \e[0m\n"
+printf "$RED[$YELLOW!$RED] An old version have been found, we will delete it ... \e[0m\n"
 cd /home/$current_user/Octopus/
 sudo rm -rf Octopus-v2.sh &>/dev/null
 sudo rm -rf README.md &>/dev/null
@@ -108,13 +114,13 @@ rm -r /home/$current_user/Octopus/ &>/dev/null
 
 # If error -->
 if [ -d /home/$current_user/Octopus ]; then
-printf "\e[31m[!] An error has occured, please restart the script ... \e[0m\n" 
+printf "$RED[$YELLOW!$RED] An error has occured, please restart the script ... \e[0m\n" 
 sleep 1
 exit
 
 # Good
 else
-printf "\e[32m[\e[33m*\e[32m] All file(s) have been deleted ... \e[0m\n"
+printf "$BLUE[$GREEN*$BLUE] All file(s) have been deleted ... \e[0m\n"
 sleep 1
 fi
 
@@ -123,10 +129,10 @@ git clone https://github.com/UnknowUser50/Octopus &>/dev/null
 cd /home/$current_user/Octopus/ &>/dev/null
 chmod 755 * 
 if [ -d /home/$current_user/Octopus ]; then
-printf "\e[32m[+] Update is complete : \e[33m$daate\e[32m \e[0m\n"
+printf "$BLUE[$GREEN+$BLUE] $GREENUpdate is complete : $BLUE$daate\e[0m\n"
 exit
 else
-printf "\e[31m[\e[33m!\e(31m] Downloading error, please restart the script ... \e[0m\n"
+printf "$RED[$YELLOW!$RED] Downloading error, please restart the script ... \e[0m\n"
 exit 
 fi
 fi
