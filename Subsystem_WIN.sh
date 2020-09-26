5e111f6cddd6146dbd8cc3d5b0983d153a06284f
UnknowUser50 <66477027+UnknowUser50@users.noreply.github.com>
2020-09-26 18:59:20 +0200

    Update Subsystem_WIN.sh

--- Subsystem_WIN.sh
+++ Subsystem_WIN.sh
@@ -25,18 +25,19 @@ hostname=$(hostname)
 
 bannerscreen() {
 
+clear
 printf "$BLUE     .=====================================================.                     \n"
 printf "$BLUE     ||                                                   ||                     \n"
 printf "$BLUE     ||$RED   _   $RED    _--'$GREEN'--_                                ||           \n"
 printf "$BLUE     ||$RED     ' $RED--''   |$GREEN    |  $RESETCOLOR .--.           |    ||      ||           \n" 
 printf "$BLUE     ||$RED   ' .  $RED_|     |$GREEN    | $RESETCOLOR |    |          |    ||      ||           \n"
 printf "$BLUE     ||$RED   _    $RED|  _--'$GREEN'--_| $RESETCOLOR |----| |.-  .-i |.-. ||      ||           \n"
-printf "$BLUE     ||    ' $BLUE --''   |$YELLOW    |  |$RESETCOLOR   | |   |  | |  |         ||         \n"
-printf "$BLUE     ||   ' $BLUE. _|     |$YELLOW    |  |$RESETCOLOR   | |    '-( |  | ()      ||         \n"
+printf "$BLUE     ||    ' $BLUE --''   |$YELLOW    |  |$RESETCOLOR   |  |   |  | |  |         ||         \n"
+printf "$BLUE     ||   ' $BLUE. _|     |$YELLOW    |  |$RESETCOLOR   |  |    '-( |  | ()      ||         \n"
 printf "$BLUE     ||    $BLUE_    |  _--'$YELLOW'--_| $RESETCOLOR            |  |              ||         \n"
 printf "$BLUE     ||    $BLUE  ' --''                 $RESETCOLOR    '--'              ||                \n"
 printf "$BLUE     ||                                                   ||                     \n"
-printf "$BLUE     `=====================================================`                     \n"
+printf "$BLUE     .=====================================================.                     \n"
 sleep 10
 
 }
