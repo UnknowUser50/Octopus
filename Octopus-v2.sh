83378b280c2c7c466f756eaa2ab1eafe29c44e50
UnknowUser50 <66477027+UnknowUser50@users.noreply.github.com>
2020-10-15 11:37:33 +0200

    Update Octopus-v2.sh

--- Octopus-v2.sh
+++ Octopus-v2.sh
@@ -261,17 +261,23 @@ read choix1
 		sudo chown $current_user domain.txt
 		printf "$BLUE [$GREEN+$BLUE] Done ! \e[0m\n"
 		sleep 2
-		if [[ -e /var/log/Octopus-Logs/subsystem.log ]]; then
-			date=$(date +%c)
-			echo "Lunch of NSLOOKUP on $domain at : $date" >> /var/log/Octopus-Logs/subsystem.log 
+		if [[ -s /home/$current_user/Octopus/Network-Infos/domain.txt ]]; then
+			printf "$RED [$YELLOW!$RED] Command error ... $RESETCOLOR \n"
 			sleep 1
-		elif [[ -e /home/$current_user/Octopus/Octopus-Logs/subsystem.log ]]; then
-			date=$(date +%c)
-			echo "Lunch of NSLOOKUP on $domain at : $date" >> /home/$current_user/Octopus-Logs/subsystem.log 
-			sleep 1
-		else 
-			printf "$RED [$YELLOW!$RED] An error as occured ... $RESETCOLOR \n"
-			sleep 1
+			mainmenu
+		else 	
+			if [[ -e /var/log/Octopus-Logs/subsystem.log ]]; then
+				date=$(date +%c)
+				echo "Lunch of NSLOOKUP on $domain at : $date result =$GREEN OK" >> /var/log/Octopus-Logs/subsystem.log 
+				sleep 1
+			elif [[ -e /home/$current_user/Octopus/Octopus-Logs/subsystem.log ]]; then
+				date=$(date +%c)
+				echo "Lunch of NSLOOKUP on $domain at : $date resulst =$GREEN OK" >> /home/$current_user/Octopus-Logs/subsystem.log 
+				sleep 1
+			else 
+				printf "$RED [$YELLOW!$RED] An error as occured ... $RESETCOLOR \n"
+				sleep 1
+			fi
 		fi	
 		mainmenu
 
