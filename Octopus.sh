c84621fccecbd3623b6d9ac0fa7001a2e5df767a
UnknowUser50 <66477027+UnknowUser50@users.noreply.github.com>
2020-10-27 10:37:50 +0100

    Help command

--- Octopus.sh
+++ Octopus.sh
@@ -2185,6 +2185,24 @@ fi
 
 }
 
+help() {
+
+clear
+printf "$RED [$YELLOW*$RED] Command : Octopus help $RESETCOLOR \n"
+printf "$BLUE [$GREEN+$BLUE] In first you may have to run the$YELLOW install.sh$BLUE script $RESETCOLOR \n"
+printf "$BLUE [$GREEN+$BLUE] If you are on a subsystem, run :$YELLOW ./Subsystem_WIN.sh$BLUE script $RESETCOLOR \n\n"
+printf "$YELLOW [$GREEN+$YELLOW] For informations about tools, go to Octopus manual $RESETCOLOR \n"
+
+exit
+
+}
+
+if [[ "$1" == "--help" ]] || [[ "$1" == "-h" ]] || [[ "$1" == "help" ]]; then
+	help
+	exit
+fi
+
+
 # Function for global script : DON'T TOUCH 
 luncher () {
 
