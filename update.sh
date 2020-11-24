e7f974540ce42b504954d1109a4c5017ca4c3311
UnknowUser50 <66477027+UnknowUser50@users.noreply.github.com>
2020-11-24 14:48:23 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-daate=$(date +%c)
 export BLUE='\033[1;94m'
 export GREEN='\033[1;92m'
 export RED='\033[1;91m'
 export YELLOW='\033[1;93m'
 export RESETCOLOR='\033[1;00m'
 path="/home/${USER}"
+back_path="/home/${USER}/Backup-Octopus/"
 
 preface() {
 
@@ -31,6 +31,9 @@ printf "$BLUE \___/ \___|\__|\__,_| .__/ \__,_|\__,_|\__\___|  \n"
 printf "$BLUE                     | |                          \n"
 printf "$BLUE                     |_|                          \n"
 printf "\n"
+echo -e -n "${BLUE}[${GREEN}+${BLUE}] Enter current user :${RESETCOLOR} "
+read current_user
+path="/home/${current_user}"
 
 }
 
@@ -47,8 +50,7 @@ if [ -d ${path}/Backup-Octopus ]; then
 
 # Moove file(s) of Network-Infos
 	if [ -d ${path}/Octopus/Network-Infos ]; then
-		cd Network-Infos/ &>/dev/null
-		sudo mv * ${path}/Backup-Octopus/Network-Infos/ &>/dev/null
+		sudo cp -r Network-Infos ${path}/Backup-Octopus/
 		cd && cd ${path}/Octopus/ &>/dev/null
 	else
 		echo -e "${RED}[${YELLOW}!${RED}] $basename$0 : internal error ! ${RESETCOLOR}" && exit 1
@@ -56,8 +58,7 @@ if [ -d ${path}/Backup-Octopus ]; then
 
 # Moove file(s) of Network-Scan
 	if [ -d ${path}/Backup-Octopus/Network-Scan ]; then
-		cd Network-Scan/ &>/dev/null
-		sudo mv * ${path}/Backup-Octopus/Network-Scan/ &>/dev/null
+		sudo cp -r Network-Scan ${path]/Backup-Octopus/ 
 		cd && cd ${path}/Octopus/ &>/dev/null
 	else
 		echo -e "${RED}[${YELLOW}!${RED}] $basename$0 : internal error ! ${RESETCOLOR}" && exit 1
@@ -65,21 +66,17 @@ if [ -d ${path}/Backup-Octopus ]; then
 
 # Moove file(s) of Web-Scan
 	if [ -d ${path}/Octopus/Web-Scan ]; then
-		cd Web-Scan/ &>/dev/null
-		sudo mv * ${path}/Backup-Octopus/Web-Scan/ &>/dev/null 
+		sudo cp -r Web-Scan ${path}/Backup-Octopus/
 		cd
 	else
 		echo -e "${RED}[${YELLOW}!${RED}] $basename$0 : internal error ! ${RESETCOLOR}" && exit 1
 	fi
 
 else
-	mkdir ${path}/Octopus &>/dev/null
-	cd ${path}/Octopus/ &>/dev/null
-	mv Network-Infos/ ${path}/Backup-Octopus/ &>/dev/null 
-	mv Network-Scan/ ${path}/Backup-Octopus/ &>/dev/null
-	mv Web-Scan/ ${path}/Backup-Octopus/ &>/dev/null
-	printf "$BLUE[$GREEN*$BLUE] All directories and files have been saved in $GREEN/%s/Backup-Octopus/ \e[0m\n" ${HOME}
-	cd ${path} &>/dev/null
+	sudo mkdir ${path}/Backup-Octopus &>/dev/null && cd ${path}/
+	cd Octopus/ ; sudo cp -r Network-Infos ${back_path} ; sudo cp -r Network-Scan ${back_path} ; sudo cp -r Web-Scan ${back_path}
+	cd ${path}/ 
+	
 fi
 
 }
@@ -89,7 +86,7 @@ update() {
 cd ${path}/ &>/dev/null
 if [ -d ${path}/Octopus ]; then
 	printf "$RED[$YELLOW!$RED] An old version have been found, we will delete it ... \e[0m\n"
-	rm -rf Octopus/ &>/dev/null
+	sudo rm -rf Octopus/ &>/dev/null
 
 	if [ -d ${path}/Octopus ]; then
 		echo -e "${BLUE}[${GREEN}+${BLUE}] All files & directories saved ${RESETCOLOR}"
